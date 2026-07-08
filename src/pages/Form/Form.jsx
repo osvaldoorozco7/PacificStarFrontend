0998a7e1bc61ef805a95a41903310f09b3a354ab
Osvaldo Orozco <osvaldoorozcorod@gmail.com>
2026-07-07 23:18:27 -0600

    Se dejó lista la estructura del proyecto

---
+++ src/pages/Form/Form.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Form = () => {
+    <div className="main-container">
+        <button>Añadir bitacora</button>
+    </div>
+}
+
+export default Form;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <h1>Pacific Star</h1>
+
+      <p>Hola</p>
+
+    </main>
+  );
+}
+
+export default Home;
